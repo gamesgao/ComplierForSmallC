a0e2f935fe9b0381a3f2deb08b960c5d8f3c310c
guyao <gamesgao@163.com>
2017-01-17 23:56:47 +0800

    补完除read外全部代码生成

--- CodeGenerate.h
+++ CodeGenerate.h
@@ -90,6 +90,56 @@ void CodeGenerate(){
     while(ptr->next != (struct Quad *)0){
         ptr = ptr->next;
         switch(ptr->op){
+            case bnot:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "not");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "%s", noneStr);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case add:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "add");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case addi:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "add");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "%d", ptr->src2.TIA);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case sub:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "sub");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case subi:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "sub");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "%d", ptr->src2.TIA);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
             case mul:{
                 if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
                 sprintf(op, "mul");
@@ -103,6 +153,26 @@ void CodeGenerate(){
             case muli:{
                 if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
                 sprintf(op, "mul");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "%d", ptr->src2.TIA);
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case opdiv:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "div");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case opdivi:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "div");
                 sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
                 sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
                 sprintf(p3, "%d", ptr->src2.TIA);
@@ -130,6 +200,17 @@ void CodeGenerate(){
                 sprintf(instLabel, "%s", noneStr);
                 break;
             }
+            case lw:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "lw");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->src1.TIA));
+                sprintf(p2, "%s", noneStr);
+                sprintf(p3, "%s($t%d)", ptr->dest.id, ptr->src2.TIA);
+                if(getsymWithinScope(ptr->dest.id, scope) != (struct symrec *) 0) CSPush(instLabel, op, p1, p2, p3, prefix);
+                else CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
             case li:{
                 if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
                 sprintf(op, "li");
@@ -151,12 +232,263 @@ void CodeGenerate(){
                 sprintf(instLabel, "%s", noneStr);
                 break;
             }
+            case sw:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "sw");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "%s", noneStr);
+                sprintf(p3, "%s($t%d)", ptr->dest.id, ptr->src2.TIA);
+                if(getsymWithinScope(ptr->dest.id, scope) != (struct symrec *) 0) CSPush(instLabel, op, p1, p2, p3, prefix);
+                else CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jgt:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "bgt");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src2.TIA));
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
             case jgti:{
                 if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
                 sprintf(op, "bgt");
                 sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
                 sprintf(p2, "%d", ptr->src2.TIA);
                 sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jge:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "bge");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src2.TIA));
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jgei:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "bge");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "%d", ptr->src2.TIA);
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jlt:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "blt");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src2.TIA));
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jlti:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "blt");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "%d", ptr->src2.TIA);
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jle:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "ble");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src2.TIA));
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jlei:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "ble");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "%d", ptr->src2.TIA);
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case je:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "beq");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src2.TIA));
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jei:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "beq");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "%d", ptr->src2.TIA);
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jne:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "bne");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src2.TIA));
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case jnei:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "bne");
+                sprintf(p1, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p2, "%d", ptr->src2.TIA);
+                sprintf(p3, "l%d", ptr->dest.addr->order);        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case band:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "and");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case bandi:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "and");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "%d", ptr->src2.TIA);
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case bxor:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "xor");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case bxori:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "xor");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "%d", ptr->src2.TIA);
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case bor:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "or");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case bori:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "or");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "%d", ptr->src2.TIA);
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case neg:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "neg");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "%s", noneStr);
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));        
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case rem:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "rem");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));     
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case remi:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "rem");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", ptr->src2.TIA);         
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case shl:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "sll");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));     
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case shli:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "sll");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", ptr->src2.TIA);         
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case shr:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "srl");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", getTemp(ptr, ptr->src2.TIA));     
+                CSPush(instLabel, op, p1, p2, p3, noneStr);
+                sprintf(instLabel, "%s", noneStr);
+                break;
+            }
+            case shri:{
+                if(ptr->basicBlockFlag == 1 && strcmp(instLabel, noneStr) == 0) sprintf(instLabel, "l%d", ptr->order);
+                sprintf(op, "srl");
+                sprintf(p1, "$t%d", getTR(ptr, ptr->dest.TIA));
+                sprintf(p2, "$t%d", getTemp(ptr, ptr->src1.TIA));
+                sprintf(p3, "$t%d", ptr->src2.TIA);         
                 CSPush(instLabel, op, p1, p2, p3, noneStr);
                 sprintf(instLabel, "%s", noneStr);
                 break;
