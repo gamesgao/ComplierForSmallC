daec7eb37d14ccc607ed388698099933e85c68ca
Gu Yao <gamesgao@gmail.com>
2017-01-20 01:01:57 +0800

    验证成功

--- ss.s
+++ ss.s
@@ -1,8 +1,9 @@
 .data
 fib.x:	.word	0
-fib.__:	.word	0
-main.__:	.word	0
+fib.__4:	.word	0
+fib.__7:	.word	0
 main.x:	.word	0
+main.__13:	.word	0
 .text
 fib:	sw	$ra		0($sp)	
 	add	$sp	$sp	-4	
@@ -17,7 +18,10 @@ l5:	li	$t1		1
 l7:	j			l22	
 l8:	lw	$t2		fib.x+0	
 	sub	$t3	$t2	1	
-	lw	$a0		fib.__+0	
+	lw	$a0		fib.__7+0	
+	sw	$a0		0($sp)	
+	add	$sp	$sp	-4	
+	lw	$a0		fib.__4+0	
 	sw	$a0		0($sp)	
 	add	$sp	$sp	-4	
 	lw	$a0		fib.x+0	
@@ -31,11 +35,17 @@ l8:	lw	$t2		fib.x+0
 	sw	$a0		fib.x+0	
 	add	$sp	$sp	4	
 	lw	$a0		0($sp)	
-	sw	$a0		fib.__+0	
-l12:	sw	$t4		fib.__+0	
+	sw	$a0		fib.__4+0	
+	add	$sp	$sp	4	
+	lw	$a0		0($sp)	
+	sw	$a0		fib.__7+0	
+l12:	sw	$t4		fib.__4+0	
 	lw	$t5		fib.x+0	
 	sub	$t6	$t5	2	
-	lw	$a0		fib.__+0	
+	lw	$a0		fib.__7+0	
+	sw	$a0		0($sp)	
+	add	$sp	$sp	-4	
+	lw	$a0		fib.__4+0	
 	sw	$a0		0($sp)	
 	add	$sp	$sp	-4	
 	lw	$a0		fib.x+0	
@@ -49,10 +59,13 @@ l12:	sw	$t4		fib.__+0
 	sw	$a0		fib.x+0	
 	add	$sp	$sp	4	
 	lw	$a0		0($sp)	
-	sw	$a0		fib.__+0	
-l17:	sw	$t7		fib.__+0	
-	lw	$t8		fib.__+0	
-	lw	$t9		fib.__+0	
+	sw	$a0		fib.__4+0	
+	add	$sp	$sp	4	
+	lw	$a0		0($sp)	
+	sw	$a0		fib.__7+0	
+l17:	sw	$t7		fib.__7+0	
+	lw	$t8		fib.__4+0	
+	lw	$t9		fib.__7+0	
 	add	$t0	$t8	$t9	
 	move	$v0	$t0		
 	add	$sp	$sp	4	
@@ -65,10 +78,10 @@ main:	sw	$ra		0($sp)
 l23:	move	$t1	$v0		
 	sw	$t1		main.x+0	
 	lw	$t2		main.x+0	
-	lw	$a0		main.x+0	
+	lw	$a0		main.__13+0	
 	sw	$a0		0($sp)	
 	add	$sp	$sp	-4	
-	lw	$a0		main.__+0	
+	lw	$a0		main.x+0	
 	sw	$a0		0($sp)	
 	add	$sp	$sp	-4	
 	sw	$t2		fib.x	
@@ -76,12 +89,12 @@ l23:	move	$t1	$v0
 	move	$t3	$v0		
 	add	$sp	$sp	4	
 	lw	$a0		0($sp)	
-	sw	$a0		main.__+0	
+	sw	$a0		main.x+0	
 	add	$sp	$sp	4	
 	lw	$a0		0($sp)	
-	sw	$a0		main.x+0	
-l28:	sw	$t3		main.__+0	
-	lw	$t4		main.__+0	
+	sw	$a0		main.__13+0	
+l28:	sw	$t3		main.__13+0	
+	lw	$t4		main.__13+0	
 	move	$a0	$t4		
 	li	$v0	1		
 	syscall				
